32fbc45735540fd1d274144d6f7c2d046f746c3c
clivern <hello@clivern.com>
2016-12-09 19:56:58 +0200

    add permissions module

---
+++ husky/permissions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    PyHusky
+    ~~~~~~
+    Python Role Based Permissions Library
+    :copyright: (c) 2016 by Clivern (hello@clivern.com).
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+class Permissions(object):
+    """Permissions Module"""
+    pass
