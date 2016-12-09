dd4c76d1784641dd8efb8dad47e6c42f939e5dea
clivern <hello@clivern.com>
2016-12-09 19:56:21 +0200

    add utils module

---
+++ husky/utils.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    PyHusky
+    ~~~~~~
+    Python Role Based Permissions Library
+    :copyright: (c) 2016 by Clivern (hello@clivern.com).
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+class Utils(object):
+    """Utils Module"""
+    pass
